2a06c0c96e592b19971ee99e57c49bb570e86b19
Abrahamn <abraham81423@gmail.com>
2022-06-29 21:59:59 -0600

    [DELETE] CARRO

--- src/Juego.asm
+++ src/Juego.asm
@@ -77,85 +77,6 @@ include File.asm
 	_bufferInfo     db 2000 dup('$'),"$"
 	contadorBuffer  dw 0 ; Contador para todos los WRITE FILE, para escribir sin que se vean los $
 
-	; ************** [JUEGO] **************
-	SPVar db " ","$"
-	TNivel db " Nivel:","$"
-	EPuntos db " Puntos:","$"
-	tabulacion db "	","$"
-	NumCero db "0","$"
-	sigDP db ":","$"
-	titulo_pausa db "                PAUSA               ","$"
-
-	;---------VARIABLES JUEGO---------
-
-	NivelPrint db 10 dup('$')
-	ValorSegundos db 0
-	SegundosAux WORD 0
-	MinutosAux WORD 0
-	PuntajeAux db 0
-	TiempoAux db 0
-	TiempoMeta WORD 0
-	NivelesCompletados WORD 0
-	ContadorNivel db 1
-	Taux WORD 0
-	TOaux WORD 0
-	TPaux WORD 0
-	PPaux WORD 0
-	POaux WORD 0
-	CCaux db 0
-	;---------------------------------
-	NivelGeneral db 1
-	ColorCarro db 13d
-	StringSize WORD 0
-	Velocidad WORD 0
-	Segundos WORD 0
-	ValorDelay WORD 10100101101b
-	ColumnaCarro WORD 10101010b
-	AnchoBarra WORD 10
-	AlturaMax WORD 150
-	AlturaAux WORD 0
-	ValorMax WORD 0
-	ColorAux db 15d
-	HzAux WORD 100d
-	InicioBarra WORD 0
-	Separacion db 0
-	Separacion2 db 0
-	SeparacionAux db 0
-	NumPrint db 100 dup('$')
-	Num db 100 dup(00h)
-
-
-
-	;**************************************************************************
-	bufferEntrada db 50 dup('$'),00
-	bufferAuxiliar db 50 dup('$')
-	handlerEntrada dw ?
-	bufferPuntos db "Puntos.rep",00h
-	handlerPuntos dw ?
-	bufferTiempo db "Tiempo.rep",00h
-	handlerTiempo dw ?
-	bufferInformacion db 300 dup('$')
-	bufferInfoAux db 200 dup('$')
-	bufferFechaHora db 15 dup('$')
-	NBytes WORD 0
-	IDAux db 10 dup('$')
-	IDAuxFile db 10 dup(00h)
-	TotalUsuarios WORD 0
-	TotalNiveles WORD 0
-	OffsetUsuario WORD 0
-	NivelAux WORD 0
-	UsuarioAux WORD 0
-	OFFSETAux WORD 0
-	Valores db 25 dup (0)
-	intAux WORD 0
-	AuxVar db 0
-	intCont WORD 0
-	NumeroAux WORD 0
-	Contador1 WORD 0
-	Contador2 WORD 0
-	Contador3 WORD 0
-	Contador4 WORD 0
-
 	; ================ SEGMENTO DE PROC ================
 	; **************************** [IDENTIFICADOR] **************************** 
 	identificador proc far
@@ -270,8 +191,7 @@ include File.asm
 
 		Lplay:
 			; clean
-			Juego	
-			RegresarATexto
+			; Juego	
 			jmp Lsalida
 
 		Lerror1:
@@ -306,88 +226,5 @@ include File.asm
 			int 21h
 
 	main endp
-
-	ConvertirNum proc
-            push bp                    ;almacenamos el puntero base
-            mov  bp,sp                 ;ebp contiene la direccion de esp
-            sub  sp,2                  ;se guarda espacio para dos variables
-            mov word ptr[bp-2],0       ;var local =0 
-            ;pusha
-            LimpiarBuffer Num, SIZEOF Num, 00h
-            xor si,si                          ;si=0
-            cmp ax,0                           ;si ax, ya viene con un cero
-            je casoMinimo           
-            mov  bx,0                          ;denota el fin de la cadena
-            push bx                            ;se pone en la pila el fin de cadena
-            Bucle:  
-                mov dx,0
-                cmp ax,0                    ;¿AX= 0?
-                je toNum                    ;si:enviar numero al arreglo                
-                mov bx,10                   ;divisor  = 10
-                div bx                      ;ax =cociente ,dx= residuo
-                add dx,48d                   ;residuo +48 para  poner el numero en ascii
-                push dx                     ;lo metemos en la pila 
-                jmp Bucle
-            toNum:
-                pop bx                      ;obtenemos elemento de la pila
-                mov word ptr[bp-2],bx       ; pasamos de 16 bits a 8 bits 
-                mov al, byte ptr[bp-2]
-                cmp al,0                    ;¿Fin de Numero?
-                je FIN                      ;si: enviar al fin del procedimiento
-                mov num[si],al              ;ponemos el numero en ascii en la cadena
-                inc si                      ;incrementamos los valores               
-                jmp toNum                   ;iteramos de nuevo            
-            casoMinimo:
-                add al,48d                         ;convertimos 0 ascii
-                mov Num[si],al                     ;Lo pasamos a num
-                jmp FIN
-            FIN:
-                ;popa
-                mov sp,bp               ;esp vuelve apuntar al inicio y elimina las variables locales
-                pop bp                  ;restaura el valor del puntro base listo para el ret
-                ret 
-    ConvertirNum endp
-
-	ConvertirPrint proc
-            push bp                   
-            mov  bp,sp                
-            sub  sp,2                 
-            mov word ptr[bp-2],0      
-            ;pusha
-            LimpiarBuffer NumPrint, SIZEOF NumPrint, 24h
-            xor si,si                        
-            cmp ax,0                        
-            je casoMinimo2         
-            mov  bx,0                       
-            push bx                          
-            Bucle2:  
-                mov dx,0
-                cmp ax,0                   
-                je toNum2                                
-                mov bx,10               
-                div bx                    
-                add dx,48d                
-                push dx                    
-                jmp Bucle2
-            toNum2:
-                pop bx                   
-                mov word ptr[bp-2],bx    
-                mov al, byte ptr[bp-2]
-                cmp al,0                   
-                je FIN2                  
-                mov NumPrint[si],al          
-                inc si                            
-                jmp toNum2                 
-            casoMinimo2:
-                add al,48d                     
-                mov NumPrint[si],al                
-                jmp FIN2
-            FIN2:
-                ;popa
-                mov sp,bp           
-                pop bp                
-                ret 
-    ConvertirPrint endp
-
 
 end main
