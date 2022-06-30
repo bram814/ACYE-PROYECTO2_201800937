c2d348c2846a406c40fd70b515b10c1b13c37c92
Abrahamn <abraham81423@gmail.com>
2022-06-29 21:48:21 -0600

    [ADD] por si acaso

--- src/Macros.asm
+++ src/Macros.asm
@@ -12,6 +12,12 @@ GetPrint macro cadena
 	int 21h
 endm
 
+print macro cadena
+	mov ah,09h
+	mov dx, offset cadena
+	int 21h
+endm
+
 
 ; ************** [CAPTURAR ENTRADA] **************
 GetInput macro 
@@ -360,3 +366,284 @@ loginVerify macro user1, compare, flag
 		pop si
 
 endm
+
+
+; ============================== VIDEO ==============================
+getCharSE macro ; sin echo
+	mov ah,08h
+	int 21h
+endm
+
+
+InicioVideo macro
+	mov ax,0013h
+	int 10h
+	mov ax, 0A000h
+	mov es, ax  ; es = A000h - MEMORIA DE LOS GRAFICOS.
+endm
+
+Delay macro constante
+	LOCAL D1,D2,Fin
+	push si
+	push di
+	mov si,constante
+	D1:
+	dec si
+	jz Fin
+	mov di,constante
+	D2:
+		dec di
+		jnz D2
+		jmp D1
+	Fin:
+		pop di
+		pop si
+endm
+
+LimpiarBuffer macro buffer, NoBytes, Char
+	LOCAL Repetir
+	xor si,si
+	xor cx,cx
+	mov cx,NoBytes
+	Repetir:
+		mov buffer[si], Char
+		inc si
+		loop Repetir
+endm
+
+DecToPrint macro NumeroDec
+    push ax     
+    mov ax,NumeroDec
+    call ConvertirPrint
+    pop ax
+endm
+
+PintarEncabezadoJuego macro
+	LOCAL PintarLinea, ImprimirConCero, ImprimirConCero2, LineaC, ImprimirSegundos
+	mov dl, 0; Column
+	mov dh, 0 ; Row
+	mov bx, 0 ; Page number, 0 for graphics modes
+	mov ah, 2h
+	int 10h
+	mov ah, 09h
+	mov al, SPVar 
+	mov bh, 00h
+	mov bl, 15d
+	mov cx, 40d
+	int 10h
+	mov dl, 0; Column
+	mov dh, 0 ; Row
+	mov bx, 0 ; Page number, 0 for graphics modes
+	mov ah, 2h
+	int 10h
+	print IDAux
+	print TNivel
+	xor cx,cx
+	mov cl,NivelGeneral
+	DecToPrint cx
+	print NumPrint
+	print EPuntos
+	xor cx,cx
+	mov cl,PuntajeAux
+	DecToPrint cx
+	print NumPrint
+	print tabulacion
+
+
+	mov cx, MinutosAux
+	cmp cx,9
+	jle ImprimirConCero
+	DecToPrint MinutosAux
+	print NumPrint
+	jmp ImprimirSegundos
+	ImprimirConCero:
+		print NumCero
+		DecToPrint MinutosAux
+		print NumPrint	
+	ImprimirSegundos:
+		print sigDP
+		mov cx,SegundosAux
+		cmp cx,9
+		jle ImprimirConCero2
+		DecToPrint SegundosAux
+		print NumPrint
+		jmp LineaC
+	ImprimirConCero2:
+		print NumCero
+		DecToPrint SegundosAux
+		print NumPrint
+	LineaC:
+		mov di,3200
+		mov cx,320
+		mov dl,9d
+	PintarLinea:
+		mov es:[di],dl
+		inc di
+		Loop PintarLinea
+endm
+
+PintarCarro macro color
+	LOCAL PrimerFor, SegundoFor
+	mov dl,color
+	mov bx,54400
+	add bx,ColumnaCarro
+	mov cx,0
+	SegundoFor:
+		mov di,bx
+		xor si,si
+	PrimerFor:
+		mov es:[di],dl
+		inc di
+		inc si
+		cmp si,20
+		jne PrimerFor
+		add bx,320
+		inc cx
+		cmp cx,25
+		jbe SegundoFor
+	endm
+
+ValidarSegundo macro
+	LOCAL SalirVS
+
+	mov  ah, 2ch
+	int  21h
+	cmp dh,ValorSegundos
+	je SalirVS
+	mov ValorSegundos,dh
+	AumentarSegundos
+
+	SalirVS:
+endm
+
+AumentarSegundos macro
+	LOCAL IncrementarMinutos, SalirAS
+	inc TiempoAux
+	inc TiempoMeta
+	inc SegundosAux
+	mov cx,SegundosAux
+	cmp cx,60
+	je IncrementarMinutos
+	jmp SalirAS
+	IncrementarMinutos:
+		inc MinutosAux
+		mov SegundosAux,0
+	SalirAS:
+endm
+
+RegresarATexto macro
+mov ah,00h
+mov al,03h
+int 10h
+endm
+
+Juego macro
+	LOCAL Reload, SumarColumna, RestarColumna, SalirJuego, Sumar, Restar, CompararValores, ReemplazarP, ReemplazarT, ContinuarExit, Pausa
+	
+	mov ColumnaCarro, 10101010b
+	mov NivelGeneral,1
+	PintarEncabezadoJuego
+	mov  ah, 2ch
+	int  21h
+	mov ValorSegundos,dh 
+	mov PuntajeAux,3
+	mov SegundosAux,0
+	mov MinutosAux,0
+	mov TiempoAux,0
+	Reload:
+		ValidarSegundo
+		PintarEncabezadoJuego
+		PintarCarro ColorCarro
+		;getCharSE
+		mov ah,11h
+		int 16h
+		jz Reload
+		mov ah,00
+		int 16h
+		cmp ah,4dh			;derecha
+		je SumarColumna
+		cmp ah,4bh			;izquierda
+		je RestarColumna
+		cmp al,1bh			;ESC
+		je Pausa
+		jmp Reload
+
+	SumarColumna:
+		mov ax, ColumnaCarro
+		cmp ax,300
+		jb Sumar
+		jmp Reload
+
+	Sumar:
+		inc PuntajeAux
+		PintarEncabezadoJuego
+		PintarCarro 0d
+		mov ax,ColumnaCarro
+		add ax,5
+		mov ColumnaCarro,ax
+		jmp Reload
+
+	RestarColumna:
+		mov ax,ColumnaCarro
+		cmp ax,0
+		ja Restar	
+		jmp Reload
+
+	Restar:
+		PintarEncabezadoJuego
+		PintarCarro 0d
+		mov ax,ColumnaCarro
+		sub ax,5
+		mov ColumnaCarro,ax
+		jmp Reload
+
+
+	Pausa:
+		mov dl, 0; Column
+		mov dh, 0 ; Row
+		mov bx, 0 ; Page number, 0 for graphics modes
+		mov ah, 2h
+		int 10h
+		mov ah, 09h
+		mov al, SPVar 
+		mov bh, 00h
+		mov bl, 15d
+		mov cx, 40d
+		int 10h
+		mov dl, 0; Column
+		mov dh, 0 ; Row
+		mov bx, 0 ; Page number, 0 for graphics modes
+		mov ah, 2h
+		int 10h
+		print titulo_pausa
+		getCharSE
+		cmp al,1bh			;ESC
+		je Reload
+		cmp al,20h			;SPACEBAR
+		je SalirJuego
+		jmp Pausa
+
+
+	; ReemplazarP:
+	; 	mov [bx].Usuario.Punteo,cl
+	; 	jmp ContinuarExit
+
+	; ReemplazarT:
+	; 	mov [bx].Usuario.Tiempo,cl
+	; 	jmp SalirJuego
+
+	; CompararValores:
+	; mov bx,UsuarioAux
+	; mov al,[bx].Usuario.Punteo
+	; mov cl,PuntajeAux
+	; cmp al,cl
+	; jl ReemplazarP
+	; ContinuarExit:
+	; 	mov al,[bx].Usuario.Tiempo
+	; 	mov cl,TiempoAux
+	; 	cmp al,cl
+	; 	jl ReemplazarT
+
+	SalirJuego:
+endm
+
